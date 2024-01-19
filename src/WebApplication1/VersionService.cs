03988c23c63e06f5026c6856d32e220f49a701fe
SeungYongShim <syshim@me.com>
2024-01-19 15:43:11 +0900

    .

--- src/WebApplication1/VersionService.cs
+++ src/WebApplication1/VersionService.cs
@@ -1,49 +1,18 @@
 
 using System.Diagnostics.Metrics;
 
-public class VersionService : IHostedService, IDisposable
+public class VersionService(IMeterFactory meterFactory) : IHostedService
 {
-    private bool disposedValue;
 
-    public Meter meter { get; init; } = new("Aums.Metric", "1.0.0");
+    public Meter Meter { get; init; } = meterFactory.Create("Aums.Metric");
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        var hatsSold = meter.CreateCounter<long>(
+        var hatsSold = Meter.CreateCounter<long>(
             name: "application.version",
             description: "application version");
-        hatsSold.Add(1, [new("version", "1.0.0")]);
+        hatsSold.Add(1, [new("app.version", "1.0.0")]);
 
         return Task.CompletedTask;
     }
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (!disposedValue)
-        {
-            if (disposing)
-            {
-                meter.Dispose();
-                Console.WriteLine("here");
-            }
-
-            // TODO: 비관리형 리소스(비관리형 개체)를 해제하고 종료자를 재정의합니다.
-            // TODO: 큰 필드를 null로 설정합니다.
-            disposedValue = true;
-        }
-    }
-
-    // // TODO: 비관리형 리소스를 해제하는 코드가 'Dispose(bool disposing)'에 포함된 경우에만 종료자를 재정의합니다.
-    // ~VersionService()
-    // {
-    //     // 이 코드를 변경하지 마세요. 'Dispose(bool disposing)' 메서드에 정리 코드를 입력합니다.
-    //     Dispose(disposing: false);
-    // }
-
-    public void Dispose()
-    {
-        // 이 코드를 변경하지 마세요. 'Dispose(bool disposing)' 메서드에 정리 코드를 입력합니다.
-        Dispose(disposing: true);
-        GC.SuppressFinalize(this);
-    }
 }
